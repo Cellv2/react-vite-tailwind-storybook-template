a0d7a12650e65b0768944f5bdcd9e4a2c3ca9b34
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2025-12-30 22:50:43 +0000

    Add prettier, opinionated formatting and test coverage

--- src/vitest-setup.ts
+++ src/vitest-setup.ts
@@ -2,6 +2,8 @@ import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 
+// TODO: global context providers : https://testing-library.com/docs/react-testing-library/setup/#global-config
+
 // https://testing-library.com/docs/react-testing-library/setup#auto-cleanup-in-vitest
 // runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
