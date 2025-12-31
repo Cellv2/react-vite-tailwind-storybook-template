0ba8512b2451c30f37e027a91112dce7c29d76e1
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2025-12-31 11:20:22 +0000

    Add storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -6,9 +9,10 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-    globalIgnores(["dist"]),
+    globalIgnores(["dist", "coverage", ".storybook"]),
     {
-        files: ["**/*.{ts,tsx}"],
+        // strict, type-aware linting actual application code
+        files: ["src/**/*.{ts,tsx}"],
         extends: [
             js.configs.recommended,
             tseslint.configs.strictTypeChecked,
@@ -18,6 +22,32 @@ export default defineConfig([
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
+            parserOptions: {
+                project: ["./tsconfig.app.json"],
+                tsconfigRootDir: process.cwd(),
+            },
+        },
+    },
+    {
+        // non-strict linting for tests, configs etc
+        files: [
+            "**/*.config.{ts,tsx}",
+            "vite.config.ts",
+            "vitest.config.ts",
+            "tests/**/*.{ts,tsx}",
+            ".storybook/**/*.{ts,tsx}", // force storybook files back in due to global ignore
+        ],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            storybook.configs["flat/recommended"],
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.node,
+            parserOptions: {
+                project: null, // disable type-aware linting
+            },
         },
     },
 ]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-// import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vitest/config";
 
@@ -6,6 +5,16 @@ import { defineConfig } from "vitest/config";
 // More info at:
 // - https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 // - https://testing-library.com/docs/react-testing-library/setup
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+import { playwright } from "@vitest/browser-playwright";
+const dirname =
+    typeof __dirname !== "undefined"
+        ? __dirname
+        : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
     plugins: [react()],
     test: {
@@ -14,6 +23,38 @@ export default defineConfig({
             provider: "v8",
         },
         environment: "jsdom",
-        setupFiles: ["./src/vitest-setup.ts"],
+        setupFiles: ["./src/vitest.setup.ts"],
+        projects: [
+            {
+                extends: true,
+                test: {
+                    name: "src",
+                },
+            },
+            {
+                extends: true,
+                plugins: [
+                    // The plugin will run tests for the stories defined in your Storybook config
+                    // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+                    storybookTest({
+                        configDir: path.join(dirname, ".storybook"),
+                    }),
+                ],
+                test: {
+                    name: "storybook",
+                    browser: {
+                        enabled: true,
+                        headless: true,
+                        provider: playwright({}),
+                        instances: [
+                            {
+                                browser: "chromium",
+                            },
+                        ],
+                    },
+                    setupFiles: [".storybook/vitest.setup.ts"],
+                },
+            },
+        ],
     },
 });
